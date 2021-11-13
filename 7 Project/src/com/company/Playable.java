9e39ae76bc37360eca67f81191dace8d86e7d0f5
Zoroaniki <92184679+Zoroaniki@users.noreply.github.com>
2021-11-13 22:52:59 +0300

    Delete 7 Project directory

--- 7 Project/src/com/company/Playable.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public interface Playable {
-    void convertStringToDeck();
-    int getDeckSize();
-    void addCard(int item);
-    int showCard();
-    int LoseCard();
-}
